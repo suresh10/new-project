b053e5f3fa8e7db14bfa7562257c2fe09fec19fe
suresh10 <suresh.m@webastitva.com>
2016-06-29 17:00:29 +0530

    Add files via upload

--- www/js/exercisetracker.js
+++ www/js/exercisetracker.js
@@ -98,24 +98,6 @@ $('#track_info').live('pageshow', function(){
   var data = window.localStorage.getItem(key);
   // Turn the stringified GPS data back into a JS object
   data = JSON.parse(data);
-});
-
-
-function gps_distance(lat1, lon1, lat2, lon2){
-  // http://www.movable-type.co.uk/scripts/latlong.html
-    var R = 6371; // km
-    var dLat = (lat2-lat1) * (Math.PI / 180);
-    var dLon = (lon2-lon1) * (Math.PI / 180);
-    var lat1 = lat1 * (Math.PI / 180);
-    var lat2 = lat2 * (Math.PI / 180);
- 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c;
-     
-    return d;
-}
 
 // Calculate the total distance travelled
 total_km = 0;
@@ -140,4 +122,25 @@ final_time_s = total_time_s - (final_time_m * 60);
  
 // Display total distance and time
 $("#track_info_info").html('Travelled <strong>' + total_km_rounded + '</strong> km in <strong>' + final_time_m + 'm</strong> and <strong>' + final_time_s + 's</strong>');
+
+});
+
+
+function gps_distance(lat1, lon1, lat2, lon2){
+  // http://www.movable-type.co.uk/scripts/latlong.html
+    var R = 6371; // km
+    var dLat = (lat2-lat1) * (Math.PI / 180);
+    var dLon = (lon2-lon1) * (Math.PI / 180);
+    var lat1 = lat1 * (Math.PI / 180);
+    var lat2 = lat2 * (Math.PI / 180);
+ 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c;
+     
+    return d;
+}
+
+
 
